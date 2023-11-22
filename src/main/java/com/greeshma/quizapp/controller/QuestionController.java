bbfee649f458ffd40ecc73497eac34363c4c80da
greeshmaSuraj2345 <151612224+greeshmaSuraj2345@users.noreply.github.com>
2023-11-22 15:32:47 +0530

    Http request

--- src/main/java/com/greeshma/quizapp/controller/QuestionController.java
+++ src/main/java/com/greeshma/quizapp/controller/QuestionController.java
@@ -4,6 +4,9 @@ package com.greeshma.quizapp.controller;
 import com.greeshma.quizapp.entity.Questions;
 import com.greeshma.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,16 +19,16 @@ public class QuestionController {
     QuestionService questionservice;
 
     @GetMapping("allQuestion")
-    public List<Questions> getAllQuestions(){
+    public ResponseEntity<List<Questions>> getAllQuestions(){
         return questionservice.getAllQuestions();
 
     }
     @GetMapping("category/{category}")
-    public List<Questions>getAllQuestionsByCategory(@PathVariable String category){
-        return questionservice.getAllQuestionsByCategory(category);
+    public ResponseEntity<List<Questions>>getAllQuestionsByCategory(@PathVariable String category){
+       return questionservice.getAllQuestionsByCategory(category);
     }
     @PostMapping("add")
-    public String addQuestion(@RequestBody Questions question){
+    public ResponseEntity<String> addQuestion(@RequestBody Questions question){
         return questionservice.addQuestion(question);
     }
 }
--- src/main/java/com/greeshma/quizapp/service/QuestionService.java
+++ src/main/java/com/greeshma/quizapp/service/QuestionService.java
@@ -3,24 +3,39 @@ package com.greeshma.quizapp.service;
 import com.greeshma.quizapp.dao.QuestionDao;
 import com.greeshma.quizapp.entity.Questions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class QuestionService {
     @Autowired
     QuestionDao questionDao;
 
-    public List<Questions> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Questions>>getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
 
-    public List<Questions> getAllQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Questions>>getAllQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
     }
 
-    public String addQuestion(Questions question) {
+    public ResponseEntity<String> addQuestion(Questions question) {
          questionDao.save(question);
-        return "success" ;
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
     }
 }
